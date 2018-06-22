b0260a775fcff662002002973300ae4721fc0d42
Prashanth Prakash <34674059+unknown-1997@users.noreply.github.com>
2018-06-22 18:41:18 +0530

    Update (7)Print the even numbers in odd rows and odd numbers in the even rows.c

--- SET 1/(7)Print the even numbers in odd rows and odd numbers in the even rows.c
+++ SET 1/(7)Print the even numbers in odd rows and odd numbers in the even rows.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
-int main(void) {
+#include <conio.h>
+int main(void) 
+{
 	int arr[]={1,2,3,4,5,6,7},i,size;
 	size=sizeof(arr)/sizeof(arr[0]);
 	for(i=0;i<size;i++)
@@ -9,7 +10,6 @@ int main(void) {
 		printf("%d ",arr[i]);
 		else if((arr[i]%2!=0)&&(i%2==0))
 		printf("%d ",arr[i]);
-		
-	}
+    }
 	return 0;
 }
